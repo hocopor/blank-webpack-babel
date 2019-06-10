d1bfe7c44ab4e734437d8631898f8f18cc524fb4
Василий Варюхин <varyukhin.v@gmail.com>
2019-06-10 09:43:29 +0300

    Add server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+import "babel-polyfill";
+
+const getBooks = async function() {
+  return fetch("//127.0.0.1:3000", {
+    mode: "no-cors",
+    headers: {
+      "Content-Type": "text/json"
+    }
+  });
+};
+
+const books = getBooks();
+
+console.log(books);
+
 //
 //  Styles
 //
