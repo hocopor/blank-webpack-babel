05fd88e58b0cade843e427ac0b5e89fde79fca2e
Василий Варюхин <varyukhin.v@gmail.com>
2019-06-10 07:19:07 +0300

    Add jest

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,11 @@ module.exports = {
           loader: "babel-loader",
           options: {
             presets: ["@babel/preset-env"],
-            plugins: ["@babel/plugin-proposal-object-rest-spread"]
+            plugins: [
+              "@babel/plugin-proposal-object-rest-spread",
+              "@babel/plugin-proposal-class-properties",
+              "@babel/plugin-proposal-private-methods"
+            ]
           }
         }
       },
